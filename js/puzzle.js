01a412e83e1315ca9c67a57340b5f3e474f840df
ShelbyFoster <Shelbia85@gmail.com>
2014-06-22 23:10:24 +0100

    Inputted puzzle / Fixed empty cells
    Inputted a real puzzle, and fixed the cells from displaying 0 when
    empty.

--- js/puzzle.js
+++ js/puzzle.js
@@ -1,17 +1,18 @@
 $(function() {
 
-	var puzzleArray = [0,0,0,0,0,2,0,0,3,
-										 0,1,0,0,0,0,0,0,0,
-										 0,0,0,0,0,0,0,0,0,
-										 0,0,0,0,0,0,0,0,0,
-										 0,0,0,0,0,0,0,0,0,
-										 0,0,0,0,0,0,0,0,0,
-										 0,0,0,0,0,0,0,0,0,
-										 0,0,0,0,0,0,0,0,0,
-						         0,0,0,0,2,0,0,0,1];
+	var puzzleArray = [3,1,0, 2,4,7, 9,8,6,
+					   9,2,4, 0,6,0, 5,0,0,
+					   0,0,0, 0,5,0, 1,0,0,
+
+					   0,0,7, 4,2,0, 8,5,1,
+					   8,5,0, 3,0,0, 7,0,2,
+					   2,0,1, 0,8,5, 6,3,0,
+
+					   0,0,0, 6,0,0, 0,0,8,
+					   1,6,0, 5,0,0, 0,0,0,
+					   0,0,0, 0,9,0, 3,0,5];
 
 	function generatePuzzle() {
-//		var linebreak = "<br />";
 		var counter = 0;
 		for(var j = 1; j < 10; j++){
 			var container = document.createElement('div');
@@ -28,10 +29,17 @@ $(function() {
 					input.maxLength = 1;
 					var start = [0,3,6,27,30,33,54,57,60];
 					var startPosition = parseInt(start[counter]);
-					input.setAttribute("placeholder", puzzleArray[startPosition+x+9*i]);
+
+                    if(puzzleArray[startPosition+x+9*i] === 0){
+                        input.setAttribute("placeholder", "");
+
+                    }
+                    else{
+                        input.setAttribute("placeholder", puzzleArray[startPosition+x+9*i]);
+                    }
+
 
 					var con = "#container" + j;
-					//console.log(con);
 					$(con).append(input);
 				}
 			}
