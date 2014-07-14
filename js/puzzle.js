4b4346e3108ee8dcb173e0ffa28a154beff023d4
ShelbyFoster <Shelbia85@gmail.com>
2014-07-14 17:26:38 +0100

    Comments/Input Values
    Added comments / corrected input values

--- js/puzzle.js
+++ js/puzzle.js
@@ -1,53 +1,66 @@
 $(function() {
 
-	var puzzleArray = [3,1,0, 2,4,7, 9,8,6,
-					   9,2,4, 0,6,0, 5,0,0,
-					   0,0,0, 0,5,0, 1,0,0,
+    var puzzleArray = [2,0,9, 0,1,0, 6,0,0,  //generated puzzle
+        0,0,4, 5,8,9, 0,0,0,
+        7,0,0, 0,3,2, 0,4,0,
 
-					   0,0,7, 4,2,0, 8,5,1,
-					   8,5,0, 3,0,0, 7,0,2,
-					   2,0,1, 0,8,5, 6,3,0,
+        0,0,7, 8,0,0, 0,6,4,
+        0,4,0, 0,0,0, 0,1,0,
+        1,6,0, 0,0,5, 8,0,0,
 
-					   0,0,0, 6,0,0, 0,0,8,
-					   1,6,0, 5,0,0, 0,0,0,
-					   0,0,0, 0,9,0, 3,0,5];
-
-	function generatePuzzle() {
-		var counter = 0;
-		for(var j = 1; j < 10; j++){
-			var container = document.createElement('div');
-			container.id = "container" + j;
-			container.className = 'container';
-			$('.grid').append(container);
+        0,2,0, 1,5,0, 0,0,9,
+        0,0,0, 3,9,8, 2,0,0,
+        0,0,6, 0,4,0, 3,0,1];
 
 
-			for(var i = 0; i < 3; i++){
-				for(var x = 0; x < 3; x++){
-					var input = document.createElement('input');
-					input.id = i;
-					input.className = 'cell';
-					input.maxLength = 1;
-					var start = [0,3,6,27,30,33,54,57,60];
-					var startPosition = parseInt(start[counter]);
+    var solvedArray = [2,8,9, 7,1,4, 6,3,5,   //solved puzzle
+        6,3,4, 5,8,9, 1,2,7,
+        7,1,5, 6,3,2, 9,4,8,
 
-                    if(puzzleArray[startPosition+x+9*i] === 0){
-                        input.setAttribute("placeholder", "");
+        3,9,7, 8,2,1, 5,6,4,
+        5,4,8, 9,6,3, 7,1,2,
+        1,6,2, 4,7,5, 8,9,3,
 
+        8,2,3, 1,5,6, 4,7,9,
+        4,7,1, 3,9,8, 2,5,6,
+        9,5,6, 2,4,7, 3,8,1];
+
+
+    function generatePuzzle() {
+        var counter = 0;    //counter for start of squares
+        var inputCounter = 0;    //id counter for inputs
+        for(var j = 1; j < 10; j++){   //create 9 divs for each square of the puzzle
+            var container = document.createElement('div');
+            container.id = "container" + j;
+            container.className = 'container';
+            $('.grid').append(container);  //append containers to the grid
+
+
+            for(var i = 0; i < 3; i++){   //iterates through rows
+                for(var x = 0; x < 3; x++){   //iterates through columns
+                    var input = document.createElement('input');
+                    input.id = inputCounter;
+                    input.className = 'cell';
+                    input.maxLength = 1;
+                    var start = [0,3,6,27,30,33,54,57,60];   //start position of each square in puzzle
+                    var startPosition = parseInt(start[counter]);   //determines the starting position of the square
+
+                    if(puzzleArray[startPosition+x+9*i] === 0){  //checks if puzzle position is empty
+                        input.value = "";   //sets input to empty
                     }
-                    else{
-                        input.setAttribute("placeholder", puzzleArray[startPosition+x+9*i]);
-                        input.setAttribute("disabled", true);
-
+                    else {
+                        input.value = puzzleArray[startPosition+x+9*i];   //if puzzle position is not empty, sets input to correct value
+                        input.setAttribute("disabled", true);   //makes the input un-editable
                     }
 
-
-					var con = "#container" + j;
-					$(con).append(input);
-				}
-			}
-			counter++;
-		}
-	}
-	generatePuzzle();
+                    var con = "#container" + j;
+                    $(con).append(input); //appends input
+                    inputCounter++;
+                }
+            }
+            counter++;
+        }
+    }
+    generatePuzzle();
 });
 
